aaee0f6b0b01479d93d0fb18d401f4225430fb62
Harris Hancock <vortrab@gmail.com>
2014-06-30 11:29:47 -0700

    Fix small bug in hash

--- include/rpc/hash.hpp
+++ include/rpc/hash.hpp
@@ -8,7 +8,8 @@ namespace rpc {
 /* Paul Larson hash */
 template <size_t N>
 constexpr uint32_t hash (const char (&s)[N], uint32_t seed = 0, size_t i = 0) {
-    return N == i ? seed : hash(s, seed * 101 + s[i], i + 1);
+    /* N - 1 so we don't hash the null terminator */
+    return (N - 1) == i ? seed : hash(s, seed * 101 + s[i], i + 1);
 }
 
 } // namespace rpc
