749e030067fe696d555f36ec39692629bef3fdd2
Harris Hancock <vortrab@gmail.com>
2014-11-05 16:15:33 -0800

    Add RemoteStatus for RPC errors on remote end

--- include/rpc/status.hpp
+++ include/rpc/status.hpp
@@ -7,27 +7,43 @@ namespace rpc {
 
 // if you edit this, make sure you keep statusToString up to date! FIXME use
 // macro magic for this.
-enum class Status {
-    OK                         = barobo_rpc_Status_OK,
-    DECODING_FAILURE           = barobo_rpc_Status_DECODING_FAILURE,
-    ENCODING_FAILURE           = barobo_rpc_Status_ENCODING_FAILURE,
-    INCONSISTENT_REQUEST       = barobo_rpc_Status_INCONSISTENT_REQUEST,
-    INCONSISTENT_REPLY         = barobo_rpc_Status_INCONSISTENT_REPLY,
-    ILLEGAL_OPERATION          = barobo_rpc_Status_ILLEGAL_OPERATION,
-    NO_SUCH_COMPONENT          = barobo_rpc_Status_NO_SUCH_COMPONENT,
-    NOT_CONNECTED              = barobo_rpc_Status_NOT_CONNECTED,
+#define rpc_Status_basic_enumeration(k) \
+    OK                         = barobo_rpc_Status_OK + k, \
+    DECODING_FAILURE           = barobo_rpc_Status_DECODING_FAILURE + k, \
+    ENCODING_FAILURE           = barobo_rpc_Status_ENCODING_FAILURE + k, \
+    INCONSISTENT_REQUEST       = barobo_rpc_Status_INCONSISTENT_REQUEST + k, \
+    INCONSISTENT_REPLY         = barobo_rpc_Status_INCONSISTENT_REPLY + k, \
+    ILLEGAL_OPERATION          = barobo_rpc_Status_ILLEGAL_OPERATION + k, \
+    NO_SUCH_COMPONENT          = barobo_rpc_Status_NO_SUCH_COMPONENT + k, \
+    NOT_CONNECTED              = barobo_rpc_Status_NOT_CONNECTED + k
 
-    UNSOLICITED_RESULT,
+enum class Status {
+    rpc_Status_basic_enumeration(0),
+
+    UNSOLICITED_REPLY,
     UNRECOGNIZED_RESULT
 };
 
+enum class RemoteStatus {
+    rpc_Status_basic_enumeration(1)
+};
+
+#undef rpc_Status_basic_enumeration
+
 const char* statusToString (Status status);
+const char* statusToString (RemoteStatus status);
 
 static_assert(0 == static_cast<int>(Status::OK),
         "Status::OK must have zero value");
+static_assert(0 != static_cast<int>(RemoteStatus::OK),
+        "RemoteStatus::OK must not have zero value");
 
 static inline bool hasError (Status s) {
     return Status::OK != s;
+}
+
+static inline bool hasError (RemoteStatus s) {
+    return RemoteStatus::OK != s;
 }
 
 } // namespace rpc
--- src/status.cpp
+++ src/status.cpp
@@ -14,10 +14,27 @@ const char* statusToString (Status status) {
         ITEM(NO_SUCH_COMPONENT);
         ITEM(NOT_CONNECTED);
 
-        ITEM(UNSOLICITED_RESULT);
+        ITEM(UNSOLICITED_REPLY);
         ITEM(UNRECOGNIZED_RESULT);
         default:
             return "(unknown status)";
+    }
+#undef ITEM
+}
+
+const char* statusToString (RemoteStatus status) {
+#define ITEM(x) case RemoteStatus::x: return "remote " #x
+    switch (status) {
+        ITEM(OK);
+        ITEM(DECODING_FAILURE);
+        ITEM(ENCODING_FAILURE);
+        ITEM(INCONSISTENT_REQUEST);
+        ITEM(INCONSISTENT_REPLY);
+        ITEM(ILLEGAL_OPERATION);
+        ITEM(NO_SUCH_COMPONENT);
+        ITEM(NOT_CONNECTED);
+        default:
+            return "(unknown remote status)";
     }
 #undef ITEM
 }
