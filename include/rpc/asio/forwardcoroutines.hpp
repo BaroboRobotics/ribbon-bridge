670c3e19cf7b5cfd27ec036210e511566edca704
Harris Hancock <harris@barobo.com>
2016-04-28 17:17:17 -0700

    Refactor the daemon in a single-threaded coroutine-based style

--- include/rpc/asio/forwardcoroutines.hpp
+++ include/rpc/asio/forwardcoroutines.hpp
@@ -5,10 +5,9 @@
 
 #include <util/asio/asynccompletion.hpp>
 #include <util/asio/operation.hpp>
+#include <util/asio/transparentservice.hpp>
 
-#include <boost/asio/async_result.hpp>
 #include <boost/asio/io_service.hpp>
-#include <boost/asio/strand.hpp>
 
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/utility/manipulators/add_value.hpp>
@@ -16,8 +15,9 @@
 
 #include <chrono>
 
-namespace rpc {
-namespace asio {
+#include <boost/asio/yield.hpp>
+
+namespace rpc { namespace asio {
 
 template <class T, class U>
 std::string to_string (const std::pair<T, U> &rid) {
@@ -28,33 +28,69 @@ std::string to_string (const std::pair<T, U> &rid) {
     return oss.str();
 }
 
-typedef void ForwardRequestsHandlerSignature(boost::system::error_code);
-typedef void ForwardBroadcastsHandlerSignature(boost::system::error_code);
-typedef void RunProxyHandlerSignature(boost::system::error_code);
+template <class Client, class Server>
+struct ProxyImpl {
+    explicit ProxyImpl (boost::asio::io_service& context)
+        : mClient(context)
+        , mServer(context)
+    {
+        mLog.add_attribute("Protocol", boost::log::attributes::constant<std::string>("RB-PX"));
+    }
+
+    void close (boost::system::error_code& ec) {
+        mClient.close(ec);
+        if (ec) {
+            BOOST_LOG(mLog) << "Ignoring client close error: " << ec.message();
+        }
+        mServer.close(ec);
+    }
+
+    Client& client () { return mClient; }
+    Server& server () { return mServer; }
+
+    auto& log () { return mLog; }
+
+    Client mClient;
+    Server mServer;
+
+    mutable boost::log::sources::logger mLog;
+};
 
 template <class C, class S>
-struct ForwardOneRequestOperation {
-    using SRequestId = typename S::RequestId;
-    using SRequestPair = typename S::RequestPair;
-    using CRequestId = typename C::RequestId;
+class Proxy : public util::asio::TransparentIoObject<ProxyImpl<C, S>> {
+public:
+    using Client = C;
+    using Server = S;
 
-    ForwardOneRequestOperation (C& client, S& server, RequestPair rp)
-        , client_(client)
-        , server_(server)
-        , serverRequestId_(rp.first)
-        , request_(rp.second)
+    explicit Proxy (boost::asio::io_service& context)
+        : util::asio::TransparentIoObject<ProxyImpl<C, S>>(context)
     {}
 
-    C& client_;
-    S& server_;
-    RequestId serverRequestId_;
-    barobo_rpc_Request request_;
+    Client& client () { return this->get_implementation()->client(); }
+    Server& server () { return this->get_implementation()->server(); }
+
+    boost::log::sources::logger& log () { return this->get_implementation()->log(); }
+};
+
+typedef void ForwardHandlerSignature(boost::system::error_code);
+
+template <class Proxy>
+struct ForwardOneRequestOperation {
+    using SRequestPair = typename Proxy::Server::RequestPair;
+    using CRequestId = typename Proxy::Client::RequestId;
+
+    explicit ForwardOneRequestOperation (Proxy& proxy, SRequestPair rp)
+        : proxy_(proxy)
+        , rp_(rp)
+    {}
+
+    Proxy& proxy_;
+
+    SRequestPair rp_;
 
     CRequestId clientRequestId_;
 
-    boost::log::sources::logger log_;
-
-    boost::system::error_code rc_ = boost::asio::error_operation_aborted;
+    boost::system::error_code rc_ = boost::asio::error::operation_aborted;
 
     std::tuple<boost::system::error_code> result () const {
         return std::make_tuple(rc_);
@@ -67,167 +103,194 @@ struct ForwardOneRequestOperation {
         using std::to_string;
         using rpc::asio::to_string;
 
-        if (!ec) reenter(op) {
-            yield client_.asyncSendRequest(requestId_, request_, std::move(op));
-            if (barobo_rpc_Request_Type_DISCONNECT == request_.type) {
-                client_.close();
-                server_.close();
+        if (!ec) reenter (op) {
+            clientRequestId_ = proxy_.client().nextRequestId();
+            yield proxy_.client().asyncSendRequest(clientRequestId_, rp_.request, std::move(op));
+            if (barobo_rpc_Request_Type_DISCONNECT == rp_.request.type) {
+                proxy_.close();
                 yield break;
             }
-            yield client_.asyncReceiveReply(requestId_, std::chrono::seconds(60), std::move(op));
+            yield proxy_.client().asyncReceiveReply(clientRequestId_,
+                std::chrono::seconds(60), std::move(op));
             if (reply) {
-                BOOST_LOG(log_) << add_value("RequestId", to_string(requestId_))
+                BOOST_LOG(proxy_.log()) << add_value("RequestId", to_string(rp_.id))
                                << "Forwarding reply to connected client";
-                yield server_.asyncSendReply(requestId_, reply, std::move(op));
+                yield proxy_.server().asyncSendReply(rp_.id, *reply, std::move(op));
             }
             else {
-                BOOST_LOG(log) << add_value("RequestId", to_string(requestId_))
+                BOOST_LOG(proxy_.log()) << add_value("RequestId", to_string(rp_.id))
                                << "Request timed out";
-                yield asyncReply(server_, requestId_, Status::TIMED_OUT, std::move(op));
+                yield asyncReply(proxy_.server(), rp_.id, Status::TIMED_OUT, std::move(op));
             }
+            rc_ = ec;
         }
-    }
-    else if (boost::asio::error::operation_aborted != ec) {
-        BOOST_LOG(log_) << "ForwardRequestsOperation I/O error: " << ec.message();
-        client_.close();
-        server_.close();
-        rc_ = ec;
+        else if (boost::asio::error::operation_aborted != ec) {
+            rc_ = ec;
+            BOOST_LOG(proxy_.log()) << "ForwardOneRequestOperation I/O error: " << ec.message();
+            proxy_.close(ec);
+            BOOST_LOG(proxy_.log()) << "Error closing proxy: " << ec.message();
+        }
     }
 };
 
-template <class C, class S, class CompletionToken>
+template <class Proxy, class CompletionToken>
 BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(boost::system::error_code))
-asyncForwardOneRequest (C& client, S& server, typename S::RequestPair rp,
+asyncForwardOneRequest (Proxy& proxy, typename Proxy::Server::RequestPair rp,
         CompletionToken&& token) {
     util::asio::AsyncCompletion<
-        CompletionToken, void(boost::system::error_code))
+        CompletionToken, void(boost::system::error_code)
     > init { std::forward<CompletionToken>(token) };
 
-    using Forwarder = ForwardOneRequestOperation<C, S>;
-    makeOperation<Forwarder>(std::move(init.handler), client, server, rp)();
+    using Op = ForwardOneRequestOperation<Proxy>;
+    util::asio::makeOperation<Op>(std::move(init.handler), proxy, rp)();
 
     return init.result.get();
 }
 
-template <class C, class S>
+template <class Proxy>
 struct ForwardRequestsOperation {
-    using RequestId = typename S::RequestId;
-    using RequestPair = typename S::RequestPair;
+    using RequestId = typename Proxy::Server::RequestId;
+    using RequestPair = typename Proxy::Server::RequestPair;
 
-    ForwardRequestsOperation (C& client, S& server)
-        , client_(client)
-        , server_(server)
+    explicit ForwardRequestsOperation (Proxy& proxy)
+        : proxy_(proxy)
     {}
 
-    C& client_;
-    S& server_;
+    Proxy& proxy_;
 
-    boost::log::sources::logger log_;
-
-    boost::system::error_code rc_ = boost::asio::error_operation_aborted;
+    boost::system::error_code rc_ = boost::asio::error::operation_aborted;
 
     std::tuple<boost::system::error_code> result () const {
         return std::make_tuple(rc_);
     }
 
     template <class Op>
-    void operator() (Op&& op, boost::system::error_code ec = {}, RequestPair arg = {}) {
-        if (!ec) reenter(op) {
+    void operator() (Op&& op, boost::system::error_code ec = {}, RequestPair rp = {}) {
+        if (!ec) reenter (op) {
             do {
-                yield server_.asyncReceiveRequest(std::move(op));
-                fork asyncForwardOneRequest(client, server, rp, op);
+                yield proxy_.server().asyncReceiveRequest(std::move(op));
+                fork asyncForwardOneRequest(proxy_, rp, Op{op});
             } while (op.is_parent());
         }
         else if (boost::asio::error::operation_aborted != ec) {
-            BOOST_LOG(log_) << "ForwardRequestsOperation I/O error: " << ec.message();
-            client_.close();
-            server_.close();
             rc_ = ec;
+            BOOST_LOG(proxy_.log()) << "ForwardRequestsOperation I/O error: " << ec.message();
+            proxy_.close(ec);
+            BOOST_LOG(proxy_.log()) << "Error closing proxy: " << ec.message();
         }
     }
 };
 
-template <class C, class S, class Handler>
-BOOST_ASIO_INITFN_RESULT_TYPE(Handler, ForwardRequestsHandlerSignature)
-asyncForwardRequests (C& client, S& server, Handler&& handler) {
+template <class Proxy, class CompletionToken>
+BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, ForwardHandlerSignature)
+asyncForwardRequests (Proxy& proxy, CompletionToken&& token) {
     util::asio::AsyncCompletion<
-        Handler, ForwardRequestsHandlerSignature
-    > init { std::forward<Handler>(handler) };
+        CompletionToken, ForwardHandlerSignature
+    > init { std::forward<CompletionToken>(token) };
 
-    using Op = ForwardRequestsOperation<C, S, decltype(init.handler)>;
-    std::make_shared<Op>(client, server)->start(init.handler);
+    using Op = ForwardRequestsOperation<Proxy>;
+    util::asio::makeOperation<Op>(std::move(init.handler), proxy)();
 
     return init.result.get();
 }
 
-template <class C, class S>
+template <class Proxy>
 struct ForwardBroadcastsOperation {
-    ForwardBroadcastsOperation (C& client, S& server)
-        : context(client.get_io_service())
-        , client_(client)
-        , server_(server)
+    explicit ForwardBroadcastsOperation (Proxy& proxy)
+        : proxy_(proxy)
     {}
 
-    C& client_;
-    S& server_;
+    Proxy& proxy_;
 
-    boost::log::sources::logger log_;
     boost::system::error_code rc_ = boost::asio::error::operation_aborted;
 
-    std::tuple<boost::system::error_code> result () {
+    std::tuple<boost::system::error_code> result () const {
         return std::make_tuple(rc_);
     }
 
     template <class Op>
     void operator() (Op&& op, boost::system::error_code ec = {},
-            barobo_rpc_Broadcast broadcast arg = {}) {
+            barobo_rpc_Broadcast arg = {}) {
         if (!ec) reenter (op) {
             while (true) {
-                yield client_.asyncReceiveBroadcast(std::move(op));
-                yield server_.asyncSendBroadcast(arg, std::move(op));
+                yield proxy_.client().asyncReceiveBroadcast(std::move(op));
+                yield proxy_.server().asyncSendBroadcast(arg, std::move(op));
             }
         }
         else if (boost::asio::error::operation_aborted != ec) {
-            BOOST_LOG(log_) << "ForwardBroadcastsOperation I/O error: " << ec.message();
-            client_.close();
-            server_.close();
             rc_ = ec;
+            BOOST_LOG(proxy_.log()) << "ForwardBroadcastsOperation I/O error: " << ec.message();
+            proxy_.close(ec);
+            BOOST_LOG(proxy_.log()) << "Error closing proxy: " << ec.message();
         }
     }
+};
 
-template <class C, class S, class CompletionToken>
-BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, ForwardBroadcastsHandlerSignature)
-asyncForwardBroadcasts (C& client, S& server, CompletionToken&& token) {
+template <class Proxy, class CompletionToken>
+BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, ForwardHandlerSignature)
+asyncForwardBroadcasts (Proxy& proxy, CompletionToken&& token) {
     util::asio::AsyncCompletion<
-        CompletionToken, ForwardBroadcastsHandlerSignature
+        CompletionToken, ForwardHandlerSignature
     > init { std::forward<CompletionToken>(token) };
 
-    using Op = ForwardBroadcastsOperation<C, S>;
-    util::asio::makeOperation<Op>(std::move(init.handler), client, server)();
+    using Op = ForwardBroadcastsOperation<Proxy>;
+    util::asio::makeOperation<Op>(std::move(init.handler), proxy)();
 
     return init.result.get();
 }
+
+template <class Proxy>
+struct RunProxyOperation {
+    explicit RunProxyOperation (Proxy& proxy)
+        : proxy_(proxy)
+    {}
+
+    Proxy& proxy_;
+
+    boost::system::error_code rc_ = boost::asio::error::operation_aborted;
+
+    std::tuple<boost::system::error_code> result () const {
+        return std::make_tuple(rc_);
+    }
+
+    template <class Op>
+    void operator() (Op&& op, boost::system::error_code ec = {}) {
+        if (!ec) reenter (op) {
+            fork Op{op}();
+            if (op.is_child()) {
+                yield asyncForwardRequests(proxy_, std::move(op));
+            }
+            else {
+                yield asyncForwardBroadcasts(proxy_, std::move(op));
+            }
+        }
+        else if (boost::asio::error::operation_aborted != ec) {
+            rc_ = ec;
+            BOOST_LOG(proxy_.log()) << "RunProxyOperation I/O error: " << ec.message();
+            proxy_.close(ec);
+            BOOST_LOG(proxy_.log()) << "Error closing proxy: " << ec.message();
+        }
+    }
+};
 
 // Forward requests from the given server to the given client, forward replies
 // and broadcasts from the client to the server. Stops only when there is an
 // error reported by either the client or the server.
-template <class C, class S, class Handler>
-BOOST_ASIO_INITFN_RESULT_TYPE(Handler, RunProxyHandlerSignature)
-asyncRunProxy (C& client, S& server, Handler&& handler) {
+template <class Proxy, class CompletionToken>
+BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, ForwardHandlerSignature)
+asyncRunProxy (Proxy& proxy, CompletionToken&& token) {
     util::asio::AsyncCompletion<
-        Handler, ForwardBroadcastsHandlerSignature
-    > init { std::forward<Handler>(handler) };
+        CompletionToken, ForwardHandlerSignature
+    > init { std::forward<CompletionToken>(token) };
 
-    WaitMultipleCompleter<decltype(init.handler)> multiHandler {
-        server.get_io_service(), init.handler
-    };
-    asyncForwardRequests(client, server, multiHandler);
-    asyncForwardBroadcasts(client, server, multiHandler);
+    using Op = RunProxyOperation<Proxy>;
+    util::asio::makeOperation<Op>(std::move(init.handler), proxy)();
 
     return init.result.get();
 }
 
-} // namespace asio
-} // namespace rpc
+}} // namespace rpc::asio
+
+#include <boost/asio/unyield.hpp>
 
 #endif
