0759917e79ce4f2f570b6b30370406abbaa46a41
Harris Hancock <harris@barobo.com>
2016-08-03 17:20:35 -0700

    Use nanopb from source

--- ribbon-bridge-functions.cmake
+++ ribbon-bridge-functions.cmake
@@ -1,60 +1,13 @@
 ##############################################################################
 # Set up nanopb
 
-if(NOT DEFINED NANOPB_ROOT)
-    if(DEFINED ENV{NANOPB_ROOT})
-        set(NANOPB_ROOT "$ENV{NANOPB_ROOT}")
-    else()
-        message(FATAL_ERROR "NANOPB_ROOT must be set")
-    endif()
-endif()
-
-file(TO_CMAKE_PATH "${NANOPB_ROOT}" NANOPB_ROOT)
-
-set(NANOPB_INCLUDE_DIR ${NANOPB_ROOT} CACHE PATH "Location of pb.h")
-
-set(PROTOC_INCLUDE_DIR
-    "${NANOPB_ROOT}/generator/proto"
-    CACHE PATH "protocol buffer compiler's include directory")
-
-set(PROTOC_EXECUTABLE
-    "${NANOPB_ROOT}/generator-bin/protoc"
-    CACHE FILEPATH "protocol buffer compiler")
-
-# nanopb_compile_proto(<c-filepath-result-var>
-#                   <h-filepath-result-var>
-#                   <proto-filepath>)
-#
-# Compile a .proto file to a .c and a .h using nanopb's protocol buffer
-# compiler. Set <c-filepath-result-var> and <h-filepath-result-var> to the
-# absolute paths of the generated .c and .h files, respectively.
-# <proto-filepath> is the input file; its path will be interpreted relative to
-# the current source directory, if it is not absolute. <c-filepath-result-var>
-# and <h-filepath-result-var>
-function(nanopb_compile_proto cFilepathResultVar hFilepathResultVar protoFilepath)
-    # Get the input .proto file's name, stripped of the last extension. Note:
-    # using get_filename_component(... NAME_WE) instead of the funky regex
-    # business would incorrectly strip all extensions. protoc only strips the
-    # last, and we want to mimic its behavior.
-    get_filename_component(_name ${protoFilepath} NAME)
-    string(REGEX REPLACE "^(.+)(\\.[^.]+)$" "\\1" _basename ${_name})
-    set(cOutput ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.pb.c)
-    set(hOutput ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.pb.h)
-
-    set(${cFilepathResultVar} ${cOutput} PARENT_SCOPE)
-    set(${hFilepathResultVar} ${hOutput} PARENT_SCOPE)
-
-    get_filename_component(_absProto ${protoFilepath} ABSOLUTE)
-    get_filename_component(_dir ${_absProto} DIRECTORY)
-
-    add_custom_command(OUTPUT ${cOutput} ${hOutput}
-        COMMAND ${PROTOC_EXECUTABLE} -I${_dir} -I${PROTOC_INCLUDE_DIR} -I${RPC_PROTO_INCLUDE_DIR}
-            --nanopb_out=${CMAKE_CURRENT_BINARY_DIR} ${_absProto}
-        MAIN_DEPENDENCY ${protoFilepath}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        COMMENT "Generating ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.pb.{c,h} from ${protoFilepath}"
-        VERBATIM)
-endfunction()
+set(oldCfrpmi ${CMAKE_FIND_ROOT_PATH_MODE_INCLUDE})
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NO_CMAKE_FIND_ROOT_PATH)
+# The CMAKE_FIND_ROOT_PATH_MODE_INCLUDE grotesquery is required to make the find_path(... pb.h)
+# call succeed in FindNanopb.cmake under cross-compilation. Resetting it via the oldCfrpmi
+# temporary variable might not strictly be required.
+find_package(Nanopb REQUIRED)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ${oldCfrpmi})
 
 # nanopb_add_proto(<target-name> <proto-filepath> [<other-source-files>...])
 #
@@ -66,13 +19,31 @@ endfunction()
 #
 # generator expression in a suitable command's arguments.
 function(nanopb_add_proto _targetName _protoFile)
-    nanopb_compile_proto(cFile hFile ${_protoFile})
-    get_filename_component(protoName ${_protoFile} NAME_WE)
-    add_library(${_targetName} STATIC ${cFile} ${ARGN})
-    get_filename_component(protoDir ${hFile} DIRECTORY)
+    list(APPEND NANOPB_IMPORT_DIRS ${RPC_PROTO_INCLUDE_DIR})
+    nanopb_generate_cpp(sources headers ${_protoFile})
+
+    if(MSVC)
+        # Disable warning C4127: conditional expression is constant
+        set_source_files_properties(${sources}
+            PROPERTIES COMPILE_FLAGS /wd4127
+        )
+    endif()
+
+    add_library(${_targetName} STATIC ${sources} ${headers} ${ARGN})
+    set(headerDirs)
+    foreach(header ${headers})
+        get_filename_component(headerDir ${header} DIRECTORY)
+        list(APPEND headerDirs ${headerDir})
+    endforeach()
+    list(REMOVE_DUPLICATES headerDirs)
     target_include_directories(${_targetName}
-        PRIVATE ${NANOPB_INCLUDE_DIR}
-        PRIVATE ${protoDir}
-        PRIVATE .)
-    set_target_properties(${_targetName} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+        PUBLIC
+            ${NANOPB_INCLUDE_DIRS}
+        PRIVATE
+            ${headerDirs}
+            .
+    )
+    if(NOT CMAKE_SYSTEM_NAME MATCHES "AVR")
+        set_target_properties(${_targetName} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+    endif()
 endfunction()
